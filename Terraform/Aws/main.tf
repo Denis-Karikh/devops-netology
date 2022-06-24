b68987b24b04e0f3d1facf872298d1021bf93ed3
Denis-Karikh <93156184+Denis-Karikh@users.noreply.github.com>
2022-06-24 15:20:36 +0400

    Update main.tf

--- Terraform/Aws/main.tf
+++ Terraform/Aws/main.tf
@@ -1,1 +1,34 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
 
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+locals {
+#  account_id = data.aws_caller_identity.current.account_id
+
+  app_instance_type_map = {
+    stage = "t1.micro"
+    prod  = "t2.micro"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = local.app_instance_type_map[terraform.workspace]
+  tags = {
+    Name = terraform.workspace
+  }
+}
